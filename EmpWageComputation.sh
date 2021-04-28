dda9a6da3f8b071756aa75b63a613a7bfa092c1f
dada4747 <adsurerahul96@gmail.com>
2021-04-28 18:00:53 +0530

    Added:welcome to empwage problem

---
+++ EmpWageComputation.sh
@@ -0,0 +1,23 @@
+#! /bin/bash -x
+echo "welcome to the employee problem"
+#Constants
+IS_PRESENT_HALF_HR=1
+IS_PRESENT_FULL_HR=2
+EMP_RATE_PER_HR=20
+
+#Variables
+empCheck=$(( RANDOM % 3 ))
+
+#Selecion
+
+if [ $empCheck -eq $IS_PRESENT_FULL_HR ]
+then
+        empHrs=8
+elif [$empCheck -eq $IS_PRESENT_FULL_HR ]
+then
+        empHrs=4
+else
+        empHrs=0
+fi
+salary = $(( empHrs * EMP_RATE_PER_HR ))
+
