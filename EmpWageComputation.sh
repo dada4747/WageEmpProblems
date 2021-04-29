0c747aab33ebc9d9ee43126f05537a476311bef1
dada4747 <adsurerahul96@gmail.com>
2021-04-29 13:30:19 +0530

    ADDED:employee Wage for Function to get work hours

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,23 +1,40 @@
-#! /bin/bash -x
-
+#! /bin/bash
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_PART_TIME=2
-WAGE_PER_HT=20
-MAX_WORKING_DAYS=20
-totalworkinghours=0
+EMP_RATE_PER_HR=20
+NO_OF_WORKING_DAYS=5
+MAX_WORK_HOURS=20
+
 totalworkingdays=0
+totalworkinghours=0
 
-while [ $totalworkingdays != $MAX_WORKING_DAYS ]
-do
-        employeecheck=$((RANDOM%3))
-        (( totalworkingdays++))
-        case $employeecheck in
-                $IS_PRESENT_FULL_TIME) empHrs=8;;
-                $IS_PRESENT_PART_TIME) empHrs=4;;
-                *) empHrs=0;;
+function  getWorkHours(){
+        local empCheck=$1
+        local empHrs=0
+        case $empCheck in
+        $IS_PRESENT_FULL_TIME ) empHrs=8 ;;
+        $IS_PRESENT_PART_TIME ) empHrs=4 ;;
+        *)
         esac
-        totalworkinghours=$(( totalworkinghours + empHrs ))
-done
-salary=$(( totalworkinghours * WAGE_PER_HR ))
-echo "$salary"
+        echo $empHrs
 
+}
+function caldailywage(){
+        emphrs=$!
+        wage=$((empHrs * EMP_RATE_PER_HR))
+        echo $wage
+}
+while [ $totalworkingdays -lt $NO_OF_WORKING_DAYS ] && [ $totalworkinghours -lt $MAX_WORK_HOURS ]
+do
+        empCheck=$(( RANDOM%3 ))
+        totalworkingdays=$(($totalworkingdays + 1 ))
+        empHrs="$( getWorkHours $empCheck )"
+#       totalworkingdays=$(( $totalworkingdays + $empHrs ))
+        empdailywage[$totalworkingdays]="$(caldailywage $empHrs)"
+done
+#salary=$(( $EMP_RATE_PER_HR * $totalworkinghours ))
+totalsalary="${caldailywage $totalworkonghours}"
+for i in ${!empdailywage[*]}
+do
+        echo $i:${empdailywage[$i]}
+done
